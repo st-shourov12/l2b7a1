d9d1c8496dd33cb07d7a6c72267efff2b113b441
st-shourov12 <shourovsc38@gmail.com>
2026-05-06 23:24:53 +0600

    solving problem seven

--- solutions.ts
+++ solutions.ts
@@ -40,5 +40,59 @@ console.log(getProperty(user, "name"));
 
 
 // problem 5 
+
+interface Book {
+    title : string;
+    author: string;
+    publishedYear : number;
+    // isRead? :  boolean ;
+}
+
+const toggleReadStatus = (obj : Book) : {} => {
+
+    return {
+        ...obj,
+        isRead : true
+    }
+}
+const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+toggleReadStatus(myBook);
+console.log(toggleReadStatus(myBook));
+
 // problem 6 
+
+class Person {
+    constructor(public Name :string , public Age : number){
+
+    }
+}
+
+class Student extends Person {
+    constructor (public Name :string , public Age : number, public Grade : string){
+        super(Name, Age)
+    }
+    getDetails () {
+        return (`Name: ${this.Name}, Age:${this.Age}, Grade:${this.Grade}`)
+    }
+}
+
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails());
+
 // problem 7 
+
+const getIntersection = (arr1 :number[], arr2:number[]) => {
+    if (arr1.length >= arr2.length) {
+       const arr = arr1.filter((item) => arr2.includes(item))
+       return arr;
+    }else if (arr2.length >= arr1.length) {
+       const arr = arr2.filter((item) => arr1.includes(item))
+       return arr;
+    }
+    
+
+}
+
+const x = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+console.log(x);
+
